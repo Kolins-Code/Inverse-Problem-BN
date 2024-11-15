13e3c8bea68bde6eb6789687ff0b93bb1eb036e1
Nikolay Mozokhin <nikolay.mozokhin@gmail.com>
2024-11-16 00:03:58 +0300

    Rewrited model with jagged array. It finally works as previous! (Except strange bounds). Ran two experiments, same as in commit before.

--- model.cs
+++ model.cs
@@ -7,6 +7,10 @@ using Range = Microsoft.ML.Probabilistic.Models.Range;
 using System.IO;
 using System.Globalization;
 using System.Diagnostics;
+using Microsoft.ML.Probabilistic.Factors;
+using System.Data;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Collections;
 
 
 class Model
@@ -22,10 +26,8 @@ class Model
 
         int size_x = (int) (end_x / dx);
         int size_t = (int) (end_t / dt);
-        //Console.WriteLine(size_t);
 
         double gamma = dt / (dx * dx);
-        //Console.WriteLine(gamma);
 
         var reader = new StreamReader(args[4]);
 
@@ -36,73 +38,88 @@ class Model
         {
             var line = reader.ReadLine();
             end_function_obs[k] = Convert.ToDouble(line, culture_param);
-            //Console.WriteLine(end_function_obs[k]);
             k++;
         }
         reader.Close();
 
 
         Range grid_x = new Range(size_x + 1).Named("Сетка по x");
+        Range grid_t = new Range(size_t + 1).Named("Сетка по t");
+        
+        VariableArray<VariableArray<double>,double[][]> solution = Variable.Array(Variable.Array<double>(grid_x), grid_t).Named("Решение прямой задачи");
+        VariableArray<double> start_function = Variable.Array<double>(grid_x).Named("Начальное условие");
+        VariableArray<double> end_function = Variable.Observed(end_function_obs);
 
-        VariableArray<double>[] solution = new VariableArray<double>[size_t + 1];
-        VariableArray<double> means = Variable.Array<double>(grid_x).Named("Средние");
-        VariableArray<double> precisions = Variable.Array<double>(grid_x).Named("Обратные дисперсии");
-        means[grid_x] = Variable.GaussianFromMeanAndVariance(0, 100).ForEach(grid_x);
-        precisions[grid_x] = Variable.GammaFromShapeAndScale(1, 1).ForEach(grid_x);
 
-        for (int i = 0; i < solution.Length; i++)
+        using (ForEachBlock x = Variable.ForEach(grid_x))
         {
-            solution[i] = Variable.Array<double>(grid_x).Named("Решение прямой задачи, слой " + i);
-        }
-        for (int i = 1; i < size_x; i++)
-        {
-            solution[0][i] = Variable.GaussianFromMeanAndPrecision(0, 0.01);
-            //solution[0][i] = Variable.GaussianFromMeanAndPrecision(means[i], precisions[i]);
-        }
-        solution[0][0] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
-        solution[0][size_x] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
-
-        for (int t = 1; t <= size_t; t++)
-        {
-            using (ForEachBlock x = Variable.ForEach(grid_x))
+            using (ForEachBlock t = Variable.ForEach(grid_t))
             {
-                using (Variable.If(x.Index == 0)) 
+                var left_cond = x.Index == 0 | x.Index == size_x;
+                //var right_cond = x.Index == size_x;
+                var start_cond = t.Index == 0;
+                var end_cond = t.Index == size_t;
+                var t_bounds = start_cond | end_cond;
+
+                using (Variable.If(left_cond)) 
                 {
-                    solution[t][x.Index] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
+                    //solution[t.Index][x.Index] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
+                    using (Variable.IfNot(end_cond)) 
+                    {
+                        solution[t.Index][x.Index] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
+                    }
                 }
-                using (Variable.IfNot(x.Index == 0)) 
+                using (Variable.IfNot(left_cond)) 
                 {
-                    using (Variable.If(x.Index == size_x)) 
+                    using (Variable.If(start_cond)) 
                     {
-                        solution[t][x.Index] = Variable.GaussianFromMeanAndPrecision(0, Double.PositiveInfinity);
+                        solution[t.Index][x.Index] = Variable.GaussianFromMeanAndVariance(0, 100);
+                      
                     }
-                    using (Variable.IfNot(x.Index == size_x)) 
+                    using (Variable.IfNot(start_cond)) 
                     {
-                        solution[t][x.Index] = (solution[t - 1][x.Index] + gamma * (solution[t][x.Index - 1] + solution[t][x.Index + 1])) / (1 + 2 * gamma) ;
+                        solution[t.Index][x.Index] = Variable.GaussianFromMeanAndVariance(0, 100);
+                        var tmp1 = (solution[t.Index][x.Index - 1] + solution[t.Index][x.Index + 1]).Named("tmp1");
+                        var tmp2 = (gamma * tmp1).Named("tmp2");
+                        var tmp3 = (solution[t.Index - 1][x.Index] + tmp2).Named("tmp3");
+                        //solution[t.Index][x.Index] = tmp3 / (1 + 2 * gamma);
+                        Variable.ConstrainEqual(solution[t.Index][x.Index], tmp3 / (1 + 2 * gamma));
+                        //solution[t.Index][x.Index] = (solution[t.Index - 1][x.Index] + gamma * (solution[t.Index][x.Index - 1] + solution[t.Index][x.Index + 1])) / (1 + 2 * gamma) ;
+                        //solution[t.Index][x.Index] = ((1 + 2 * gamma) * solution[t.Index][x.Index - 1] - solution[t.Index - 1][x.Index - 1] - gamma * solution[t.Index][x.Index - 2]) / gamma;
+                        /*var tmp1 = ((1 + 2 * gamma) * solution[t.Index][x.Index - 1]).Named("tmp1");
+                        var tmp2 = (tmp1 - - solution[t.Index - 1][x.Index - 1]).Named("tmp2");
+                        var tmp3 = (tmp2 - gamma * solution[t.Index][x.Index - 2]).Named("tmp3");
+                        solution[t.Index][x.Index] = tmp3 / gamma;*/
                     }
+                    
                     
                 }
             }  
         }
 
-
-        solution[size_t].ObservedValue = end_function_obs;
+        using (ForEachBlock x = Variable.ForEach(grid_x))
+        {
+            end_function[x.Index] = solution[size_t][x.Index];
+        }
+        using (ForEachBlock x = Variable.ForEach(grid_x))
+        {
+            start_function[x.Index] = solution[0][x.Index];
+        }
 
 
         InferenceEngine engine = new InferenceEngine();
         engine.SaveFactorGraphToFolder = "graphs";
-        engine.NumberOfIterations = 200;
+        engine.NumberOfIterations = 50;
+        //engine.Compiler.BrowserMode = BrowserMode.Always;
 
-        var prediction = engine.Infer<DistributionStructArray<Gaussian, double>>(solution[0]);
-        //Console.WriteLine(prediction);
+        var prediction = engine.Infer<DistributionStructArray<Gaussian, double>>(start_function);
+        Console.WriteLine(prediction);
         double[] output_function = new double[size_x + 1];
         int index = 0;
         foreach (var point in prediction)
         {
             output_function[index++] = point.GetMean();
         }
-        //Console.WriteLine(engine.Infer<DistributionStructArray<Gaussian, double>>(means));
-        //Console.WriteLine(engine.Infer<DistributionStructArray<Gamma, double>>(precisions));
 
         using (StreamWriter writer = new StreamWriter(args[5]))
         {
